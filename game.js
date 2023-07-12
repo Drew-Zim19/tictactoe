7a43eb5fa131a43af1c5597f33bf160bf6362505
Drew Zimmerman <drewjacobzimmerman@gmail.com>
2023-07-12 12:34:53 -0400

    Add winner function to determine if there is a win

--- game.js
+++ game.js
@@ -28,27 +28,32 @@ const Game = (() => {
     let board = gameBoard.board;
     let validInput = true;
     let currentPlayer = player1;
+    let isWinner = false;
 
+//adds click handlers to grab the player selections and process them
     const spots = Array.from(document.getElementsByClassName('box'));
         spots.forEach((box) => {
             box.addEventListener('click', (event) => {
                 validInput = checkBoxValidClick(event);
                 if(validInput == true){
                     addMark(currentPlayer, board, event);
-                   currentPlayer = togglePlayer(currentPlayer);
-                    console.log(board);
-                    console.log(currentPlayer);
-                    //determineWinner(board);
+                   isWinner = determineWinner(board, currentPlayer, isWinner);
+                   if(isWinner == true){
+                    alert("Winner!");
+                   }
+                    currentPlayer = togglePlayer(currentPlayer);
+                    
+                    
                 }
                 else{
                     alert("This box has already been played!")
                 }
             });
         });
-
+ //function which checks for valid selection in an empty square       
     function checkBoxValidClick(event){
         boxClicked = Number(event.target.getAttribute("data-index"));
-        if(gameBoard.board[boxClicked - 1] == '')
+        if(gameBoard.board[boxClicked] == '')
         {
             return true;
         }
@@ -56,13 +61,13 @@ const Game = (() => {
             return false;
         }
     }
-
+//function which adds the given players symbol
     function addMark(player, board, event){
        let boxClicked = Number(event.target.getAttribute("data-index"));
-       board[boxClicked - 1] = player.symbol;
+       board[boxClicked] = player.symbol;
         return board;
     }
-
+//function which toggles the player making their move
     function togglePlayer(currentPlayer){
         if(currentPlayer == player1){
             currentPlayer = player2;
@@ -72,6 +77,29 @@ const Game = (() => {
         }
         return currentPlayer;
     }
+
+function determineWinner(board, player, isWinner){
+let winningCombos =     [
+                    [0, 1, 2], 
+                    [0,3,6], 
+                    [0,4,8],
+                    [1,4,7],
+                    [2,5,8],
+                    [2,4,6], 
+                    [3,4,5], 
+                    [6,7,8]
+                            ];
+    for(i=0; i<winningCombos.length; i++){
+        const [a, b, c] = winningCombos[i];
+        if(board[a] && board[a] === board[b] && board[a] === board[c]){
+            isWinner = true;
+        }
+    }
+    console.log(board);
+    return isWinner;
+}
+
+
 
     return{
         startGame,
